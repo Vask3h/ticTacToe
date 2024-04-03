4b9decba9c916d353e19c963deaf9f55e65c12c4
Daniel <danielvazquez245.dv@gmail.com>
2024-04-03 15:58:26 +0200

    v1 finished

--- scripts.js
+++ scripts.js
@@ -2,13 +2,13 @@ let player1Name = "";
 let player2Name = "";
 let turn = true;
 let screenInfo = "Tic Tac Toe";
-
 function saveNames(){
     player1Name = document.getElementById('p1Name').value;
     player2Name = document.getElementById('p2Name').value;
 }
 
 function board(pos){
+
     if(document.getElementById(pos).innerHTML === "" ){
         if(turn === true){
             document.getElementById(pos).innerHTML = "x";
@@ -34,21 +34,21 @@ function board(pos){
         document.getElementById('screenInfo').innerHTML = "The winner is! " + player1Name;
     }else if(document.getElementById('pos3').innerHTML === "x" && document.getElementById('pos5').innerHTML === "x" && document.getElementById('pos7').innerHTML === "x"){
         document.getElementById('screenInfo').innerHTML = "The winner is! " + player1Name;
-    }else if (document.getElementById('pos1').innerHTML === "0" && document.getElementById('pos2').innerHTML === "0" && document.getElementById('pos3').innerHTML === "0"){ //player 2
+    }else if (document.getElementById('pos1').innerHTML === "o" && document.getElementById('pos2').innerHTML === "o" && document.getElementById('pos3').innerHTML === "o"){ //player 2
         document.getElementById('screenInfo').innerHTML = "The winner is! " + player2Name;
-    }else if(document.getElementById('pos4').innerHTML === "0" && document.getElementById('pos5').innerHTML === "0" && document.getElementById('pos6').innerHTML === "0"){
+    }else if(document.getElementById('pos4').innerHTML === "o" && document.getElementById('pos5').innerHTML === "o" && document.getElementById('pos6').innerHTML === "o"){
         document.getElementById('screenInfo').innerHTML = "The winner is! " + player2Name;
-    }else if(document.getElementById('pos7').innerHTML === "0" && document.getElementById('pos8').innerHTML === "0" && document.getElementById('pos9').innerHTML === "0"){
+    }else if(document.getElementById('pos7').innerHTML === "o" && document.getElementById('pos8').innerHTML === "o" && document.getElementById('pos9').innerHTML === "o"){
         document.getElementById('screenInfo').innerHTML = "The winner is! " + player2Name;
-    }else if(document.getElementById('pos1').innerHTML === "0" && document.getElementById('pos4').innerHTML === "0" && document.getElementById('pos7').innerHTML === "0"){
+    }else if(document.getElementById('pos1').innerHTML === "o" && document.getElementById('pos4').innerHTML === "o" && document.getElementById('pos7').innerHTML === "o"){
         document.getElementById('screenInfo').innerHTML = "The winner is! " + player2Name;
-    }else if(document.getElementById('pos2').innerHTML === "0" && document.getElementById('pos5').innerHTML === "0" && document.getElementById('pos8').innerHTML === "0"){
+    }else if(document.getElementById('pos2').innerHTML === "o" && document.getElementById('pos5').innerHTML === "o" && document.getElementById('pos8').innerHTML === "o"){
         document.getElementById('screenInfo').innerHTML = "The winner is! " + player2Name;
-    }else if(document.getElementById('pos3').innerHTML === "0" && document.getElementById('pos6').innerHTML === "0" && document.getElementById('pos9').innerHTML === "0"){
+    }else if(document.getElementById('pos3').innerHTML === "o" && document.getElementById('pos6').innerHTML === "o" && document.getElementById('pos9').innerHTML === "o"){
         document.getElementById('screenInfo').innerHTML = "The winner is! " + player2Name;
-    }else if(document.getElementById('pos1').innerHTML === "0" && document.getElementById('pos5').innerHTML === "0" && document.getElementById('pos9').innerHTML === "0"){
+    }else if(document.getElementById('pos1').innerHTML === "o" && document.getElementById('pos5').innerHTML === "o" && document.getElementById('pos9').innerHTML === "o"){
         document.getElementById('screenInfo').innerHTML = "The winner is! " + player2Name;
-    }else if(document.getElementById('pos3').innerHTML === "0" && document.getElementById('pos5').innerHTML === "0" && document.getElementById('pos7').innerHTML === "0"){
+    }else if(document.getElementById('pos3').innerHTML === "o" && document.getElementById('pos5').innerHTML === "o" && document.getElementById('pos7').innerHTML === "o"){
         document.getElementById('screenInfo').innerHTML = "The winner is! " + player2Name;
     }else if(document.getElementById('pos1').innerHTML != "" && document.getElementById('pos2').innerHTML != "" && document.getElementById('pos3').innerHTML != "" 
     && document.getElementById('pos4').innerHTML != "" && document.getElementById('pos5').innerHTML != "" && document.getElementById('pos6').innerHTML != "" 
@@ -58,8 +58,8 @@ function board(pos){
     }
     
 function restart(pos){
-    let player1Name = "";
-    let player2Name = "";
+    player1Name = "Player 1";
+    player2Name = "Player 2";
     document.getElementById('screenInfo').innerHTML = "Tic Tac Toe"
     turn = true;
     for(let i=1; i<10; i++){
