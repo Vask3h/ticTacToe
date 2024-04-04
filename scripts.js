87c954d79da184ba1eddf2330d3f3326cca681a9
Daniel <danielvazquez245.dv@gmail.com>
2024-04-04 11:04:17 +0200

    v1.1 Finished

--- scripts.js
+++ scripts.js
@@ -2,16 +2,22 @@ let player1Name = "";
 let player2Name = "";
 let turn = true;
 let screenInfo = "Tic Tac Toe";
-let winner = "noWinner";
+let draw = "noWinner";
+let winner = "";
 let p1Wins = 0;
 let p2Wins = 0;
+
+
+
 function saveNames(){
     player1Name = document.getElementById('p1Name').value;
     player2Name = document.getElementById('p2Name').value;
+    document.getElementById('name1').innerHTML = player1Name;
+    document.getElementById('name2').innerHTML = player2Name;
 }
 
 function board(pos){
-    if(winner != ""){
+    if(draw != ""){
         if(document.getElementById(pos).innerHTML === "" ){
             if(turn === true){
                 document.getElementById(pos).innerHTML = "x";
@@ -23,67 +29,97 @@ function board(pos){
         }
         if(document.getElementById('pos1').innerHTML === "x" && document.getElementById('pos2').innerHTML === "x" && document.getElementById('pos3').innerHTML === "x"){
             document.getElementById('screenInfo').innerHTML = "The winner is! " + player1Name;
+            p1Wins = p1Wins + 1 ;
+            document.getElementById('p1Wins').innerHTML = p1Wins ;
             winner = player1Name;
         }else if(document.getElementById('pos4').innerHTML === "x" && document.getElementById('pos5').innerHTML === "x" && document.getElementById('pos6').innerHTML === "x"){
             document.getElementById('screenInfo').innerHTML = "The winner is! " + player1Name;
+            p1Wins = p1Wins + 1 ;
+            document.getElementById('p1Wins').innerHTML = p1Wins ;
             winner = player1Name;
         }else if(document.getElementById('pos7').innerHTML === "x" && document.getElementById('pos8').innerHTML === "x" && document.getElementById('pos9').innerHTML === "x"){
             document.getElementById('screenInfo').innerHTML = "The winner is! " + player1Name;
+            p1Wins = p1Wins + 1 ;
+            document.getElementById('p1Wins').innerHTML = p1Wins ;
             winner = player1Name;
         }else if(document.getElementById('pos1').innerHTML === "x" && document.getElementById('pos4').innerHTML === "x" && document.getElementById('pos7').innerHTML === "x"){
             document.getElementById('screenInfo').innerHTML = "The winner is! " + player1Name;
+            p1Wins = p1Wins + 1 ;
+            document.getElementById('p1Wins').innerHTML = p1Wins ;
             winner = player1Name;
         }else if(document.getElementById('pos2').innerHTML === "x" && document.getElementById('pos5').innerHTML === "x" && document.getElementById('pos8').innerHTML === "x"){
             document.getElementById('screenInfo').innerHTML = "The winner is! " + player1Name;
+            p1Wins = p1Wins + 1 ;
+            document.getElementById('p1Wins').innerHTML = p1Wins ;
+            p1Wins = p1Wins + 1 ;
+            document.getElementById('p1Wins').innerHTML = p1Wins ;
             winner = player1Name;
         }else if(document.getElementById('pos3').innerHTML === "x" && document.getElementById('pos6').innerHTML === "x" && document.getElementById('pos9').innerHTML === "x"){
             document.getElementById('screenInfo').innerHTML = "The winner is! " + player1Name;
+            p1Wins = p1Wins + 1 ;
+            document.getElementById('p1Wins').innerHTML = p1Wins ;
             winner = player1Name;
         }else if(document.getElementById('pos1').innerHTML === "x" && document.getElementById('pos5').innerHTML === "x" && document.getElementById('pos9').innerHTML === "x"){
             document.getElementById('screenInfo').innerHTML = "The winner is! " + player1Name;
+            p1Wins = p1Wins + 1 ;
+            document.getElementById('p1Wins').innerHTML = p1Wins ;
             winner = player1Name;
         }else if(document.getElementById('pos3').innerHTML === "x" && document.getElementById('pos5').innerHTML === "x" && document.getElementById('pos7').innerHTML === "x"){
             document.getElementById('screenInfo').innerHTML = "The winner is! " + player1Name;
+            p1Wins = p1Wins + 1 ;
+            document.getElementById('p1Wins').innerHTML = p1Wins ;
             winner = player1Name;
         }else if (document.getElementById('pos1').innerHTML === "o" && document.getElementById('pos2').innerHTML === "o" && document.getElementById('pos3').innerHTML === "o"){ //player 2
             document.getElementById('screenInfo').innerHTML = "The winner is! " + player2Name;
+            p2Wins = p2Wins + 1 ;
+            document.getElementById('p2Wins').innerHTML = p2Wins ;
             winner = player2Name;
         }else if(document.getElementById('pos4').innerHTML === "o" && document.getElementById('pos5').innerHTML === "o" && document.getElementById('pos6').innerHTML === "o"){
             document.getElementById('screenInfo').innerHTML = "The winner is! " + player2Name;
+            p2Wins = p2Wins + 1 ;
+            document.getElementById('p2Wins').innerHTML = p2Wins ;
             winner = player2Name;
         }else if(document.getElementById('pos7').innerHTML === "o" && document.getElementById('pos8').innerHTML === "o" && document.getElementById('pos9').innerHTML === "o"){
             document.getElementById('screenInfo').innerHTML = "The winner is! " + player2Name;
+            p2Wins = p2Wins + 1 ;
+            document.getElementById('p2Wins').innerHTML = p2Wins ;
             winner = player2Name;
         }else if(document.getElementById('pos1').innerHTML === "o" && document.getElementById('pos4').innerHTML === "o" && document.getElementById('pos7').innerHTML === "o"){
             document.getElementById('screenInfo').innerHTML = "The winner is! " + player2Name;
+            p2Wins = p2Wins + 1 ;
+            document.getElementById('p2Wins').innerHTML = p2Wins ;
             winner = player2Name;
         }else if(document.getElementById('pos2').innerHTML === "o" && document.getElementById('pos5').innerHTML === "o" && document.getElementById('pos8').innerHTML === "o"){
             document.getElementById('screenInfo').innerHTML = "The winner is! " + player2Name;
+            p2Wins = p2Wins + 1 ;
+            document.getElementById('p2Wins').innerHTML = p2Wins ;
             winner = player2Name;
         }else if(document.getElementById('pos3').innerHTML === "o" && document.getElementById('pos6').innerHTML === "o" && document.getElementById('pos9').innerHTML === "o"){
             document.getElementById('screenInfo').innerHTML = "The winner is! " + player2Name;
+            p2Wins = p2Wins + 1 ;
+            document.getElementById('p2Wins').innerHTML = p2Wins ;
             winner = player2Name;
         }else if(document.getElementById('pos1').innerHTML === "o" && document.getElementById('pos5').innerHTML === "o" && document.getElementById('pos9').innerHTML === "o"){
             document.getElementById('screenInfo').innerHTML = "The winner is! " + player2Name;
+            p2Wins = p2Wins + 1 ;
+            document.getElementById('p2Wins').innerHTML = p2Wins ;
             winner = player2Name;
         }else if(document.getElementById('pos3').innerHTML === "o" && document.getElementById('pos5').innerHTML === "o" && document.getElementById('pos7').innerHTML === "o"){
             document.getElementById('screenInfo').innerHTML = "The winner is! " + player2Name;
+            p2Wins = p2Wins + 1 ;
+            document.getElementById('p2Wins').innerHTML = p2Wins ;
             winner = player2Name;
         }else if(document.getElementById('pos1').innerHTML != "" && document.getElementById('pos2').innerHTML != "" && document.getElementById('pos3').innerHTML != "" 
         && document.getElementById('pos4').innerHTML != "" && document.getElementById('pos5').innerHTML != "" && document.getElementById('pos6').innerHTML != "" 
         && document.getElementById('pos7').innerHTML != "" && document.getElementById('pos8').innerHTML != "" && document.getElementById('pos9').innerHTML != ""){
            document.getElementById('screenInfo').innerHTML = "No one wins!!";
-           winner = "noWinner";
+           draw = "noWinner";
         }
         }
     }
     
 function restart(pos){
-    player1Name = "Player 1";
-    player2Name = "Player 2";
-    p1Wins = 0;
-    p2Wins = 0;
-    winner = "noWinner";
+
     document.getElementById('screenInfo').innerHTML = "Tic Tac Toe"
     turn = true;
     for(let i=1; i<10; i++){
